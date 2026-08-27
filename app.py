8446c2e61c2c20c5b9242be6dcb4dd5baab6cde3
Rico Ardiansyah <festyus09@gmail.com>
2026-08-27 23:05:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,12 +37,10 @@ with gr.Blocks(
     
 def launch_gradio(server_name: str, server_port: int) -> None:
     app.launch(
-        favicon_path="assets/logo.ico",
         share="--share" in sys.argv,
         inbrowser="--open" in sys.argv,
         server_name=server_name,
         server_port=server_port,
-        show_error=True,
     )
 
 
