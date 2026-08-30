6095c26df4019aad43addde57da3e469f6f830d6
Rico Ardiansyah <festyus09@gmail.com>
2026-08-31 03:19:10 +0700

    Update download.py

--- tabs/download.py
+++ tabs/download.py
@@ -1,8 +1,70 @@
 from rvc.modules.model_manager import download_from_url, upload_separate_files, upload_zip_file
 import gradio as gr
+import yt_dlp
+import os, sys
+
+BASE_DIR = sys.path.append(os.getcwd())
+
+output_dir = os.path.join(BASE_DIR, 'song_output')
+
+def raise_exception(error_msg):
+    raise gr.Error(error_msg)
+    raise Exception(error_msg)
+
 
 app_name = "Polgen RVC"
 url_github = "https://github.com/Bebra777228/PolGen-RVC"
+
+
+def get_youtube_video_id(url, ignore_playlist=True):
+    query = urlparse(url)
+    if query.hostname == 'youtu.be':
+        if query.path[1:] == 'watch':
+            return query.query[2:]
+        return query.path[1:]
+
+    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
+        if not ignore_playlist:
+            with suppress(KeyError):
+                return parse_qs(query.query)['list'][0]
+        if query.path == '/watch':
+            return parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/watch/':
+            return query.path.split('/')[1]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+
+    return None
+
+
+def yt_download(link):
+    ydl_opts = {
+        'format': 'bestaudio',
+        'outtmpl': '%(title)s',
+        'nocheckcertificate': True,
+        'ignoreerrors': True,
+        'no_warnings': True,
+        'quiet': True,
+        'extractaudio': True,
+        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(link, download=True)
+        download_path = ydl.prepare_filename(result, outtmpl='%(title)s.mp3')
+
+    if urlparse(song_input).scheme == 'https':
+        input_type = 'yt'
+        song_id = get_youtube_video_id(song_input)
+        if song_id is None:
+            error_msg = 'Invalid YouTube URL.'
+            raise_exception(error_msg)
+
+    return download_path
+
+
+
 
 
 def dltabs():
